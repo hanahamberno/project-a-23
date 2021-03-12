dcbc6f063241e08f5e2044ba319a027d55e138e0
hjk6281 <hjk6281@gmail.com>
2021-03-12 03:10:56 -0500

    modified url and home.html, about.html

--- social_app/urls.py
+++ social_app/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+app_name = 'social_app'
+
 urlpatterns = [
-    path('', views.home, name="roommate-home"),
+    path('', views.home, name="home"),
+    path('about', views.about, name="about"),
 ]
--- social_app/views.py
+++ social_app/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'social_app/index.html')
+    return render(request, 'social_app/home.html')
+
+
+def about(request):
+    return render(
+        request=request, 
+        template_name='social_app/about.html'
+    )
