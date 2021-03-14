0412b7239e6c941ae6a6c4b0e89d507bb0f97149
hjk6281 <hjk6281@gmail.com>
2021-03-14 17:35:06 -0400

    initial travis work

--- RoommateFinder/urls.py
+++ RoommateFinder/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import path, include # <--
-from django.views.generic import TemplateView # <--
+# from django.views.generic import TemplateView # <--
 
 urlpatterns = [
-    path('', include("social_app.urls")),
+    path('', include("social_app.urls", namespace="social_app")),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')), # <--
 ]
