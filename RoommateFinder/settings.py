f80b31e2029b5ec061f078ce62de8866066774e2
Kathy Kang <kck3due@virginia.edu>
2021-03-24 09:54:00 -0400

    added whitenoise to middleware

--- RoommateFinder/settings.py
+++ RoommateFinder/settings.py
@@ -93,6 +93,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'RoommateFinder.urls'
