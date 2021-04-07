ce1fe5cdf66fdc97e19362cffd108df3ca6e956d
hjk6281 <hjk6281@gmail.com>
2021-04-07 17:47:38 -0400

    copied and pasted profile_update view to property_update view

--- users/views.py
+++ users/views.py
@@ -34,13 +34,35 @@ def profile(request):
     return render(request, 'users/profile.html', context) 
 
 def property_update(request):
-    #if request.method == 'POST':
-        # prop_form = PropertyUpdateForm(
-        #     request.POST,
-        #     request.FILES,
-        #     instance= something_idk)
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = PropertyUpdateForm(request.POST,
+                                   request.FILES,
+                                   #it is profile.property because it's OneToOne field
+                                   instance=request.user.profile)
+        prop_form = PropertyUpdateForm(request.POST,
+                                       request.FILES,
+                                       instance = request.profile.property #fix this
+                                       )
+        if u_form.is_valid() and p_form.is_valid() and prop_form.is_valid(): #and prop_form.is_valid():
+            u_form.save()
+            p_form.save()
+            prop_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+        prop_form = PropertyUpdateForm(instance = request.profile.property) #fix this
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form,
+        'prop_form' : prop_form
+    }
     return render(
         request=request,
         template_name='users/property_update.html',
-        context={},
+        context=context,
     )
