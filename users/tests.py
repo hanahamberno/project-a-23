b0621fd681c7a7fb4042fe5ace4709bb7665b06c
Nathan Snyder <nbs9vy@virginia.edu>
2021-04-11 22:16:52 -0400

    fixed transaction management error in tests

--- users/tests.py
+++ users/tests.py
@@ -1,30 +1,40 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from users.models import *
+from django.db import transaction
 
 # Create your tests here.
 
 
 class ProfileModelTest(TestCase):
+    # (Nathan)
     def setUp(self):
-        User.objects.create_user(
-            username='test_user',
-            password='test_user',
-            email='12345@gmail.com'
-        )
+        try:
+            with transaction.atomic():
+                test_user = User.objects.create_user(
+                    username='test_user',
+                    password='test_user',
+                    email='123456@gmail.com'
+                )
+        except:
+            test_user = User.objects.filter(username='test_user')[0]
 
-        Profile.objects.create(
-            user=User.objects.filter(username='test_user')[0],
-            image="default.jpg",
-            age=19,
-            bio="hello",
-            graduation_year=2023,
-            pronouns="she/they",
-            phone_number="472-555-0291",
-            on_grounds=Profile.ON_GROUNDS,
-            max_price=1500,
-            display_profile=True
-        )
+        try:
+            with transaction.atomic():
+                Profile.objects.create(
+                    user=test_user,
+                    image="default.jpg",
+                    age=19,
+                    bio="hello",
+                    graduation_year=2023,
+                    pronouns="she/they",
+                    phone_number="472-555-0291",
+                    on_grounds=Profile.ON_GROUNDS,
+                    max_price=1500,
+                    display_profile=True
+                )
+        except:
+            pass
 
     # (Seungeon)
     # a test method to check if the __str__ method returns the correct username
@@ -33,12 +43,12 @@ class ProfileModelTest(TestCase):
         # (Seungeon) objects.get() should be unique
         # this method should be used only when you know this attritube is unique, such as id value
         # so .filter() returns the queryset, something like list, and we can index them.
-        test_user1 = User.objects.filter(username='test_user')[0]
-        test_profile1 = Profile.objects.filter(user__username='test_user')[0]
+        test_user = User.objects.filter(username='test_user')[0]
+        test_profile = Profile.objects.filter(user__username='test_user')[0]
         # (Seungeon) str() will call __str__() inside the Profile Model
-        self.assertEqual(str(test_user1), 'test_user')
-        self.assertEqual(str(test_profile1),
-                         f'{test_profile1.user.username} Profile')
+        self.assertEqual(str(test_user), 'test_user')
+        self.assertEqual(str(test_profile), f'{test_profile.user.username} Profile')
+    
 
     # (Seungeon)
     # Django validation is mostly application level validation and not validation at DB level
@@ -46,34 +56,34 @@ class ProfileModelTest(TestCase):
     # If you want to validate your values at certain time in your code then you need to do it manually
     # ref: https://stackoverflow.com/questions/40881708/django-model-validator-not-working-on-create
     def test_profile_valid_graduation_year(self):
-        test_profile1 = Profile.objects.filter(user__username='test_user')[0]
-        test_profile1.graduation_year = 2022
+        test_profile = Profile.objects.filter(user__username='test_user')[0]
+        test_profile.graduation_year = 2022
         try:
             print("Testing valid graduation year(2022)\n")
-            test_profile1.full_clean()
-            self.assertTrue(test_profile1.is_valid_graduation_year())
+            test_profile.full_clean()
+            self.assertTrue(test_profile.is_valid_graduation_year())
             # Model.full_clean() validates the model fields, the field uniqueness
         except ValidationError:
             print("'test_profile_valid_graaduation_year'failed")
         else:
-            test_profile1.save()
+            test_profile.save()
 
     # (Seungeon)
     # Try inserting invalid graduation_year into model
     # It should raise ValidationError
     def test_profile_invalid_graduation_year(self):
-        test_profile1 = Profile.objects.filter(user__username='test_user')[0]
-        test_profile1.graduation_year = 2040
+        test_profile = Profile.objects.filter(user__username='test_user')[0]
+        test_profile.graduation_year = 2040
         try:
             print("Testing invalid graduation year(2040)\n")
-            test_profile1.full_clean()
+            test_profile.full_clean()
         except ValidationError:
             pass
         else:
             print("'test_profile_invalid_graduation_year' failed")
 
     # def testUserCanLogin(self):
-    #     user = User.objects.get(username='test_user')
+    #     user = User.objects.get(username='test_user1')
     #     profile = Profile.objects.get(user=user)
     #     print("helloooo")
     #     self.assertEqual(1,1)
