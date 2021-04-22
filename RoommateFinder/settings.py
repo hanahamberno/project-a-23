11f3f75d323153ee7a87a128e683fadc792080d8
Nathan Snyder <nbs9vy@virginia.edu>
2021-04-22 08:23:06 -0400

    undid changes

--- RoommateFinder/settings.py
+++ RoommateFinder/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'bnsa+wj-r#obp6!bdnc$(@yr41$g3jsj%163g2w(dyfgtv%bep'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '0.0.0.0',
@@ -261,6 +261,6 @@ TWILIO_API_SECRET='7QlAPapGXtOQdAdwH3Zbu87bYJGt4Ocq'
 
 
 
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+#SECURE_SSL_REDIRECT = True
+#SESSION_COOKIE_SECURE = True
+#CSRF_COOKIE_SECURE = True
