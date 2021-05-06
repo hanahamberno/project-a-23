724c952fb9e1b5fe53b85c5d754c68871db676f8
Nathan Snyder <nbs9vy@virginia.edu>
2021-05-05 22:46:25 -0400

    cleaned up models.py and tests.py and added tests for number of roommates seeking

--- users/tests.py
+++ users/tests.py
@@ -32,13 +32,17 @@ class ProfileModelTest(TestCase):
         except:
             pass
 
+
+
     def test_profile_str(self):
         print("Testing profile __str__() method...")
         test_user = User.objects.filter(username='test_user')[0]
         test_profile = Profile.objects.filter(user__username='test_user')[0]
         self.assertEqual(str(test_user), 'test_user')
         self.assertEqual(str(test_profile), f'{test_profile.user.username} Profile')
-    
+
+
+
     def test_profile_valid_graduation_year(self):
         print("Testing profile valid graduation year...")
         test_profile = Profile.objects.filter(user__username='test_user')[0]
@@ -50,43 +54,49 @@ class ProfileModelTest(TestCase):
         test_profile = Profile.objects.filter(user__username='test_user')[0]
         test_profile.graduation_year = 2040
         self.assertFalse(test_profile.is_valid_graduation_year())
-    
+
+
+
     def test_profile_valid_age(self):
         print("Testing profile valid age...")
         test_profile = Profile.objects.filter(user__username='test_user')[0]
         test_profile.age = 19
         self.assertTrue(test_profile.is_valid_age())
-    
+
     def test_profile_invalid_age(self):
         print("Testing profile invalid age...")
         test_profile = Profile.objects.filter(user__username='test_user')[0]
         test_profile.age = 2040
         self.assertFalse(test_profile.is_valid_age())
-    
+
+
+
     def test_profile_valid_phone_number(self):
         print("Testing profile valid phone number...")
         test_profile = Profile.objects.filter(user__username='test_user')[0]
         test_profile.phone_number="472-555-0291"
         self.assertTrue(test_profile.is_valid_phone_number())
-    
+
     def test_profile_invalid_phone_number(self):
         print("Testing profile invalid phone number...")
         test_profile = Profile.objects.filter(user__username='test_user')[0]
         test_profile.phone_number="472-555-02914"
         self.assertFalse(test_profile.is_valid_phone_number())
-    
+
+
+
     def test_profile_valid_max_price(self):
         print("Testing profile valid max price...")
         test_profile = Profile.objects.filter(user__username='test_user')[0]
         test_profile.max_price = 1500
         self.assertTrue(test_profile.is_valid_max_price())
-    
+
     def test_profile_invalid_max_price(self):
         print("Testing profile invalid max price...")
         test_profile = Profile.objects.filter(user__username='test_user')[0]
         test_profile.max_price = -500
         self.assertFalse(test_profile.is_valid_max_price())
-    
+
     # 0 should be a valid price
     def test_profile_edge_case_max_price(self):
         print("Testing profile edge case max price...")
@@ -129,7 +139,7 @@ class PropertyModelTest(TestCase):
                 test_profile.save()
         except:
             test_profile = Profile.objects.filter(user=test_user)[0]
-        
+
         try:
             with transaction.atomic():
                 test_property = Property.objects.create(
@@ -142,10 +152,13 @@ class PropertyModelTest(TestCase):
         except:
             pass
 
+
+
     def test_property_str(self):
         print("Testing property __str__() method...")
         test_property = Property.objects.filter(profile__user__username='test_user')[0]
         self.assertEqual(str(test_property), f'{test_property.profile.user.first_name} {test_property.profile.user.last_name}\'s Property')
+
 
 
     def test_property_valid_rent(self):
@@ -166,22 +179,47 @@ class PropertyModelTest(TestCase):
         test_property = Property.objects.filter(profile__user__username='test_user')[0]
         test_property.rent = 0
         self.assertTrue(test_property.is_valid_rent())
-    
-    def test_property_valid_num_roommates(self):
-        print("Testing property valid num roommates...")
+
+
+
+    def test_property_valid_curr_num_roommates(self):
+        print("Testing property valid curr num roommates...")
         test_property = Property.objects.filter(profile__user__username='test_user')[0]
         test_property.current_number_of_roommates = 2
-        self.assertTrue(test_property.is_valid_num_roommates())
+        self.assertTrue(test_property.is_valid_curr_num_roommates())
 
-    def test_property_invalid_num_roommates(self):
-        print("Testing property invalid num roommates...")
+    def test_property_invalid_curr_num_roommates(self):
+        print("Testing property invalid curr num roommates...")
         test_property = Property.objects.filter(profile__user__username='test_user')[0]
         test_property.current_number_of_roommates = 34
-        self.assertFalse(test_property.is_valid_num_roommates())
-    
+        self.assertFalse(test_property.is_valid_curr_num_roommates())
+
     # 0 is a valid current number of roommates
-    def test_property_edge_case_num_roommates(self):
-        print("Testing property edge case num roommates...")
+    def test_property_edge_case_curr_num_roommates(self):
+        print("Testing property edge case curr num roommates...")
         test_property = Property.objects.filter(profile__user__username='test_user')[0]
         test_property.current_number_of_roommates = 0
-        self.assertTrue(test_property.is_valid_rent())
+        self.assertTrue(test_property.is_valid_curr_num_roommates())
+
+
+
+    def test_property_valid_num_roommates_seeking(self):
+        print("Testing property valid num roommates seeking...")
+        test_property = Property.objects.filter(profile__user__username='test_user')[0]
+        test_property.number_of_roommates_seeking = 2
+        self.assertTrue(test_property.is_valid_num_roommates_seeking())
+
+    # 0 is a NOT a valid number of roommates seeking
+    def test_property_invalid_roommates_seeking(self):
+        print("Testing property invalid num roommates seeking...")
+        test_property = Property.objects.filter(profile__user__username='test_user')[0]
+        test_property.number_of_roommates_seeking = 0
+        self.assertFalse(test_property.is_valid_num_roommates_seeking())
+
+    # 1 is a valid current number of roommates
+    def test_property_edge_case_roommates_seeking(self):
+        print("Testing property edge case num roommates seeking...")
+        test_property = Property.objects.filter(profile__user__username='test_user')[0]
+        test_property.number_of_roommates_seeking = 1
+        self.assertTrue(test_property.is_valid_num_roommates_seeking())
+
